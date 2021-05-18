30a7890e1e32e12fad5414c68adc7f34523ea183
jose700 <58452692+jose700@users.noreply.github.com>
2021-05-18 12:04:24 -0500

    Update mensaje.js

--- mensaje.js
+++ mensaje.js
@@ -1,4 +1,25 @@
- function myFunction() {
-     var enviar = "Te has registrado exitosamente";
-     alert(enviar);
- }
+//funcion para enviar un mensaje una vez registrado el usuario
+function myFunction() {
+    var enviar = "Te has registrado exitosamente";
+    alert(enviar);
+}
+/*funcion para poder visualizar la contraseña al hacer click en el icono
+agregamos una condicion para poder hacer la vizualizacion viceverza
+*/
+function mostrarPassword() {
+    var cambio = document.getElementById("txtPassword");
+    if (cambio.type == "password") {
+        cambio.type = "text";
+        $('.icon').removeClass('fa fa-eye-slash').addClass('fa fa-eye');
+    } else {
+        cambio.type = "password";
+        $('.icon').removeClass('fa fa-eye').addClass('fa fa-eye-slash');
+    }
+}
+
+$(document).ready(function() {
+    //CheckBox mostrar contraseña
+    $('#ShowPassword').click(function() {
+        $('#Password').attr('type', $(this).is(':checked') ? 'text' : 'password');
+    });
+});
